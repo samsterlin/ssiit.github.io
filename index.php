72507036f97fcc1794c9a0fa2df0f71460fbbe00
Sam Sterlin S <samsterlin@gmail.com>
2025-07-09 20:35:06 +0530

    SSIIT whats app icon added in homepage

--- index.php
+++ index.php
@@ -739,6 +739,25 @@
                 gap: 1rem;
             }
         }
+    
+        .float{
+            position:fixed;
+            width:60px;
+            height:60px;
+            bottom:40px;
+            right:40px;
+            background-color:#25d366;
+            color:#FFF;
+            border-radius:50px;
+            text-align:center;
+        font-size:30px;
+            box-shadow: 2px 2px 3px #999;
+        z-index:100;
+        }
+
+        .my-float{
+            margin-top:16px;
+        }
     </style>
 </head>
 <body>
@@ -1067,6 +1086,11 @@
             </div>
         </div>
     </footer>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<a href="https://api.whatsapp.com/send?phone=+919486003799&text=Hello! I would like more information about SSIIT..?" class="float" target="_blank">
+<i class="fa fa-whatsapp my-float"></i>
+</a>   
 
     <script>
         // Mobile menu toggle
